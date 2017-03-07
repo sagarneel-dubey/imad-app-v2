ebaa4531bd473c4649fa96ba2fe3b97040af64be
sagarneel-dubey <sagarneel.dubey@gmail.com>
2017-03-08 00:29:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+
+var config = {
+    user: 'sagarneel-dubey',
+    database: 'sagarneel-dubey',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB-PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -69,7 +79,7 @@ function createTemplate(data){
         </body>
     </html> `;
     return htmlTemplate;
-};
+}
 
 
 var counter = 0;
@@ -84,6 +94,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+  pool.query('SELECT * FROM article', function(err,result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringiy(result));
+      }
+  }
+});
 
 app.get('/counter', function (req, res) {
   var counterString = incrementCounter().toString();
